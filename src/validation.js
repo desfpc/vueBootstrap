a9c2deaf47231028331a6bb18c27b8d57e551ee4
Sergey Peshalov <desfpc@gmail.com>
2023-06-27 06:48:54 +0400

    Refactoring validation.js to plugin (in work)

--- src/validation.js
+++ src/validation.js
@@ -1,66 +1,52 @@
 export default {
-    data() {
-        return {
-            validationRules: {
-                string: {
-                    validate(val) {
-                        if (val !== null && val.length > 0) {
-                            return {valid: true, error: ''}
-                        } else {
-                            return {valid: false, error: 'Empty string'}
-                        }
-                    },
-                },
-                positiveInteger: {
-                    validate(val) {
-                        let intVal = parseInt(val)
+    install: (app, options) => {
 
-                        if (isNaN(intVal)) {
-                            return {valid: false, error: 'Not an integer'}
-                        }
-
-                        if (intVal !== null && intVal >= 0) {
-                            return {valid: true, error: ''}
-                        } else {
-                            return {valid: false, error: 'Not an integer or negative integer'}
-                        }
-                    },
-                },
-                positiveIntegerNotZero: {
-                    validate(val) {
-                        let intVal = parseInt(val)
-
-                        if (isNaN(intVal)) {
-                            return {valid: false, error: 'Not an integer'}
-                        }
-
-                        if (intVal !== null && intVal > 0) {
-                            return {valid: true, error: ''}
-                        } else {
-                            return {valid: false, error: 'Not an integer, negative integer or zero'}
-                        }
-                    },
+        app.config.globalProperties.$validationRules = {
+            string: {
+                validate(val) {
+                    if (val !== null && val.length > 0) {
+                        return {valid: true, error: ''}
+                    } else {
+                        return {valid: false, error: 'Empty string'}
+                    }
                 },
             },
-            validationArr: {}
+            positiveInteger: {
+                validate(val) {
+                    let intVal = parseInt(val)
+
+                    if (isNaN(intVal)) {
+                        return {valid: false, error: 'Not an integer'}
+                    }
+
+                    if (intVal !== null && intVal >= 0) {
+                        return {valid: true, error: ''}
+                    } else {
+                        return {valid: false, error: 'Not an integer or negative integer'}
+                    }
+                },
+            },
+            positiveIntegerNotZero: {
+                validate(val) {
+                    let intVal = parseInt(val)
+
+                    if (isNaN(intVal)) {
+                        return {valid: false, error: 'Not an integer'}
+                    }
+
+                    if (intVal !== null && intVal > 0) {
+                        return {valid: true, error: ''}
+                    } else {
+                        return {valid: false, error: 'Not an integer, negative integer or zero'}
+                    }
+                },
+            },
         }
-    },
 
-    methods: {
-        globalValidation(id, value) {
-            this.validationArr[id] = value
-        },
+        app.config.globalProperties.$validationArr = {}
 
-        checkValidationArr() {
-            let result = true;
-
-            for (const [key, value] of Object.entries(this.validationArr)) {
-                if (key !== null && key !== 'null' && !value) {
-                    return false
-                }
-            }
-
-            return result;
-        },
+        app.config.globalProperties.$globalValidation = (id, value) => {
+            app.config.globalProperties.$validationArr[id] = value
+        }
     }
 }
